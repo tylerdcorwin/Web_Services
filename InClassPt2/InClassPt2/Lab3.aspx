1fc4852a55720045ac2cd95928bb69ac59f50866
Corwin <tdcorwin@labs.neit.edu>
2017-10-19 21:16:15 -0400

    updated lab3

---
+++ InClassPt2/InClassPt2/Lab3.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Lab3.aspx.cs" Inherits="InClassPt2.Lab3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Get Weather Forecast for Boston<br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Get Boston Weather" />
+    
+    </div>
+        <asp:TextBox ID="TextBox1" runat="server" Height="241px" TextMode="MultiLine" Width="605px"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ InClassPt2/InClassPt2/Lab3.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace InClassPt2
+{
+    public partial class Lab3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest myHttpWebRequest = null;     //Declare an HTTP-specific implementation of the WebRequest class.
+            HttpWebResponse myHttpWebResponse = null;   //Declare an HTTP-specific implementation of the WebResponse class
+            XmlTextReader myXMLReader = null;           //Declare XMLReader           
+            XPathNavigator nav;
+            XPathDocument docNav;
+            XPathNodeIterator NodeIter;
+            XPathNodeIterator NodeIter2;
+            //Create Request 
+
+            String WeatherURL = "http://api.wunderground.com/api/69b2a5c4bee93799/forecast/q/MA/Boston.xml";
+
+            myHttpWebRequest = (HttpWebRequest)HttpWebRequest.Create(WeatherURL);
+            myHttpWebRequest.Method = "GET";
+            myHttpWebRequest.ContentType = "text/xml; encoding='utf-8'";
+            //Get Response
+            myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+
+            //Load response stream into XMLReader
+            myXMLReader = new XmlTextReader(myHttpWebResponse.GetResponseStream());
+
+            docNav = new XPathDocument(myXMLReader);
+            // Create a navigator to query with XPath.
+            nav = docNav.CreateNavigator();
+
+            string fcstDay = "//forecastday";
+            NodeIter = nav.Select(fcstDay);
+
+            string feels = "//forecastday";
+            NodeIter2 = nav.Select(feels);
+
+            while (NodeIter.MoveNext())
+            {
+                TextBox1.Text += NodeIter.Current.SelectSingleNode("title");
+                TextBox1.Text += "\n";
+                TextBox1.Text += NodeIter.Current.SelectSingleNode("fcttext");
+                TextBox1.Text += "\n";
+            }
+            /*
+            while (NodeIter2.MoveNext())
+            {
+                lbResults.Items.Add(NodeIter2.Current.SelectSingleNode("fcttext").Value);
+            }
+            */
+
+        }
+    }
+}
