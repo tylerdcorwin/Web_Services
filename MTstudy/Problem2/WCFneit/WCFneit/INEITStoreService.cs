4ac3b9d0149b61ebef3748abde76178a2a71ae25
Tyler <tydcorwin@gmail.com>
2017-11-09 14:53:47 -0500

    Midterm study guide

---
+++ MTstudy/Problem2/WCFneit/WCFneit/INEITStoreService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFneit
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface INEITStoreService
+    {
+
+
+        /*
+        Steps of renameing files
+        - Create WCF application
+        - Rename Service1.svc
+        - Replace class name (of what you renamed to) in Service1.svc.cs
+        - Rename IService1.cs (same name as rename + I)
+        - GoTo Markup for Service1.svc
+        - Change service file after namespace
+        - Run Project to make sure its working
+        - Start Coding!
+        */
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
