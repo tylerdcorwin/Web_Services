cb2a71c94b659329c705c80dab1fb89e24ad6bd6
Corwin <tdcorwin@labs.neit.edu>
2017-11-09 20:01:02 -0500

    added problem 3

---
+++ NEITStoreOrdersService/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.NEITStoreOrdersServiceClient myProxy = new ServiceReference1.NEITStoreOrdersServiceClient();
+
+            Console.WriteLine("----- Bank Information ------");
+
+            Console.WriteLine("Bank Account ID: " + myProxy.GetBankInformation("12345").accountID);
+            Console.WriteLine("Bank Account name: " + myProxy.GetBankInformation("12345").accountName);
+            Console.WriteLine("Account Type: " + myProxy.GetBankInformation("12345").account[0].accountType);
+            Console.WriteLine("Free Checking: " + myProxy.GetBankInformation("12345").account[0].options.FreeChecking);
+            Console.WriteLine("Overdraft Protection: " + myProxy.GetBankInformation("12345").account[0].options.OverDraftProtection);
+
+            Console.WriteLine();
+            Console.WriteLine("Hit ENTER to exit");
+            Console.ReadLine();
+        }
+    }
+}
