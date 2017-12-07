d0afb9910443869f78df606f0c7545bf36ecb5de
Tyler <tydcorwin@gmail.com>
2017-12-07 11:07:54 -0500

    FinalProject Commit

---
+++ MTstudy/Problem2/SnowReporter/SnowReporter/default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="SnowReporter._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h4>To Number:</h4>
+        <asp:TextBox ID="ToNumber" TextMode="Password" runat="server"></asp:TextBox>
+        <h4>Message:</h4>
+        <asp:TextBox ID="Message" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="SendMessage" runat="server" Text="Send Message" OnClick="SendMessage_OnClick" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ MTstudy/Problem2/SnowReporter/SnowReporter/default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Configuration;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using Twilio.Clients;
+
+namespace SnowReporter
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void SendMessage_OnClick(object sender, EventArgs e)
+        {
+            const string accountSid = "AC1aea9d222ae314d10dfde35c6369c189";
+            const string authToken = "f5edc8b98a6c3175aea89f29394274cd";
+            TwilioClient.Init(accountSid, authToken);
+
+            var to = new PhoneNumber("+14017148826"); //This will be from txt box with augmentation
+            var message = MessageResource.Create(to,
+                                                 //+15005550006  +17748723011
+                                                 from: new PhoneNumber("+17748723011"),
+                                                 body: "Testing this out");
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
